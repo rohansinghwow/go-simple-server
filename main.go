aad55b8ae85421a67880f27efbe2b6a5939af42d
Rohan Singh <rohan.singh@codemonk.in>
2023-02-28 11:28:04 +0530

    feat: /form route

--- main.go
+++ main.go
@@ -24,4 +24,22 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if r.Method != "GET" {
+		http.Error(w, "method not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello!")
+
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
 }
